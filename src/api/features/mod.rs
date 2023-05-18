8f10ff062ecfc5727c1a0de23f8f7aa705202a53
Dom Rodriguez <shymega@shymega.org.uk>
2023-05-18 21:25:11 +0100

    feat(FeatureKind): Add more Feature variants, and other things
    - Set #[repr(i32)] attr.
    - Add more features.
    - Tidy up documentation.
    - Rename Enum.

--- src/api/features/mod.rs
+++ src/api/features/mod.rs
@@ -3,20 +3,31 @@
 /// This is an enum of commands for the earbuds.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
-pub enum FeatureVariant {
+#[repr(i32)]
+pub enum FeatureKind {
     /// Feature that can be sent to the earbuds, to check that the command handler is
     /// running.
     /// Payload received should be 0x01.
     GetMorphFeatureHandlerVersion = 0x00,
     /// Feature that can be sent to the earbuds, to check the volume.
-    /// Volume received from this command is payload 0x30, or 48 in decimal.
     /// (Possible range of volume is 0-0x7f, or 0-127 in decimal)
     GetVolume = 0x01,
+    /// Change volume.
+    /// Accept payload, 0x40 is example. In hex.
+    SetVolume(i32) = 0x02,
+    /// Toggle microphone mute.
+    ToggleMicMute = 0x03,
+    /// Get microphone state.
+    GetMicMuteState = 0x04,
+    /// Switch earbuds into pairing mode.
+    SetStartPairingMode = 0x05,
+    /// Get connection status of headset.
+    GetIsDeviceConnected = 0x06,
     /// Default variant.
     Undefined,
 }
 
-impl Default for FeatureVariant {
+impl Default for FeatureKind {
     fn default() -> Self {
         Self::Undefined // Default variant.
     }
