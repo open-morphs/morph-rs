9943216496cdff77cd06d789e0dc424d7424cff1
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-09 00:24:17 +0100

    Add `consts` module with UUIDs for BLE on Morph

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -4,6 +4,14 @@
 mod enums;
 pub use enums::*;
 
+mod consts {
+    use uuid::{uuid, Uuid};
+
+    const MORPH_SERVICE_UUID: Uuid = uuid!("00001100-D102-11E1-9B23-00025B00A5A5");
+    const MORPH_COMMAND_UUID: Uuid = uuid!("00001101-D102-11E1-9B23-00025B00A5A5");
+    const MORPH_RESPONSE_UUID: Uuid = uuid!("00001102-D102-11E1-9B23-00025B00A5A5");
+}
+
 /// `Message` acts as the final representation of a BLE packet to the earbuds.
 #[derive(Debug, Clone)]
 pub struct Message {
